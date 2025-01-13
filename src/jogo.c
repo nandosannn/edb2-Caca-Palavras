bac034ad302b0d11a0300fccc294c3d8fbbbfe1b
gustavo.olliveira49@gmail.com <gustavo.olliveira49@gmail.com>
2025-01-13 02:06:30 -0300

    Funções de busca no tabuleiro implementadas

--- src/jogo.c
+++ src/jogo.c
@@ -34,46 +34,337 @@ void ler_tabuleiro(char *nomeDoArquivo, int linhas, int colunas, char **matriz){
             } 
         }
     }
-
-    for(int i = 0; i < linhas; i++){
-        for(int j = 0; j < colunas; j++){
-            printf("%c", matriz[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
 }
 
 void busca_horizontal(char** matriz, int linha, int coluna, No_trie* arvore_trie, No** arvore_avl){
     char palavra[50];
-    int posicao_de_insercao_do_caractere = 0;
+    int posicao_insercao_caracter = 0;
     int posicao_inicial_palavra = 0;
 
-    for (int i = 0; i < linha; i++)
-    {
-        while (posicao_inicial_palavra < coluna){
+    for(int i = 0; i < linha; i++){
+
+        while(posicao_inicial_palavra < coluna){
            
-            for (int j = posicao_inicial_palavra; j < coluna; j++)
-            {
-                palavra[posicao_de_insercao_do_caractere++] = matriz[i][j];
-                palavra[posicao_de_insercao_do_caractere] = '\0';
+            for(int j = posicao_inicial_palavra; j < coluna; j++){
+
+                palavra[posicao_insercao_caracter++] = matriz[i][j];
+                palavra[posicao_insercao_caracter] = '\0';
                 int valor = buscar_trie(arvore_trie, palavra);
 
-                if(valor == 1)
-                {
+                if(valor == 1){
                     *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
                     j = coluna;
-                }else if(valor == -1)
-                {
-                    j = coluna;
-                }
+                }else if(valor == -1) j = coluna;
             }
 
             palavra[0] = '\0';
-            posicao_de_insercao_do_caractere = 0;
+            posicao_insercao_caracter = 0;
             posicao_inicial_palavra++;
         }
 
         posicao_inicial_palavra = 0;
     } 
+
+    posicao_inicial_palavra = coluna - 1;
+    posicao_insercao_caracter = 0;
+
+    for(int i = 0; i < linha; i++){
+
+        while(posicao_inicial_palavra >= 0){
+           
+            for (int j = posicao_inicial_palavra; j >= 0; j--){
+
+                palavra[posicao_insercao_caracter++] = matriz[i][j];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    j = -1;
+                }else if(valor == -1) j = -1;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra--;
+        }
+
+        posicao_inicial_palavra = coluna - 1;
+    } 
+}
+
+void busca_vertical(char** matriz, int linha, int coluna, No_trie* arvore_trie, No** arvore_avl){
+    char palavra[50];
+    int posicao_insercao_caracter = 0;
+    int posicao_inicial_palavra = 0;
+
+    for(int i = 0; i < coluna; i++){
+
+        while(posicao_inicial_palavra < linha){
+           
+            for(int j = posicao_inicial_palavra; j < linha; j++){
+
+                palavra[posicao_insercao_caracter++] = matriz[j][i];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if(valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    j = linha;
+                }else if(valor == -1) j = linha;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra++;
+        }
+
+        posicao_inicial_palavra = 0;
+    } 
+
+    posicao_inicial_palavra = linha - 1;
+    posicao_insercao_caracter = 0;
+
+    for(int i = 0; i < coluna; i++){
+
+        while(posicao_inicial_palavra >= 0){
+           
+            for (int j = posicao_inicial_palavra; j >= 0; j--){
+
+                palavra[posicao_insercao_caracter++] = matriz[j][i];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    j = -1;
+                }else if(valor == -1) j = -1;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra--;
+        }
+
+        posicao_inicial_palavra = linha - 1;
+    } 
+}
+
+void busca_diagonal(char** matriz, int coluna, int linha, No_trie* arvore_trie, No** arvore_avl){
+    char palavra[50];
+    int posicao_insercao_caracter = 0;
+    int posicao_inicial_palavra = 0;
+
+    for(int h = 0; h < linha; h++){
+
+        while(posicao_inicial_palavra < linha){
+
+            for(int l = posicao_inicial_palavra; l < linha - h; l++){
+
+                palavra[posicao_insercao_caracter++] = matriz[l + h][l];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    l = linha;
+                }else if(valor == -1) l = linha;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra++;
+        }
+
+        posicao_inicial_palavra = 0;
+    }
+
+    posicao_inicial_palavra = linha - 1;
+    posicao_insercao_caracter = 0;
+
+    for(int h = 0; h < linha; h++){
+
+        while(posicao_inicial_palavra >= 0){
+
+            for(int l = posicao_inicial_palavra; l - h >= 0; l--){
+
+                palavra[posicao_insercao_caracter++] = matriz[l][l - h];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    l = -1;
+                }else if(valor == -1) l = -1;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra--;
+        }
+
+        posicao_inicial_palavra = linha - 1;
+    }
+
+    posicao_insercao_caracter = 0;
+    posicao_inicial_palavra = 0;
+
+    for(int h = 1; h < linha; h++){
+
+        while(posicao_inicial_palavra < linha){
+
+            for(int l = posicao_inicial_palavra; l < linha - h; l++){
+
+                palavra[posicao_insercao_caracter++] = matriz[l][l + h];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    l = linha;
+                }else if(valor == -1) l = linha;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra++;
+        }
+
+        posicao_inicial_palavra = 0;
+    }
+
+    posicao_inicial_palavra = linha - 1;
+    posicao_insercao_caracter = 0;
+
+    for(int h = 1; h < linha; h++){
+
+        while(posicao_inicial_palavra >= 0){
+
+            for(int l = posicao_inicial_palavra; l - h >= 0; l--){
+
+                palavra[posicao_insercao_caracter++] = matriz[l - h][l];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    l = -1;
+                }else if(valor == -1) l = -1;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra--;
+        }
+
+        posicao_inicial_palavra = linha - 1;
+    }
+
+    posicao_insercao_caracter = 0;
+    posicao_inicial_palavra = 0;
+
+    for(int h = coluna - 1; h >= 0; h--){
+
+        while(posicao_inicial_palavra < coluna){
+
+            for(int c = posicao_inicial_palavra; c <= h; c++){
+
+                palavra[posicao_insercao_caracter++] = matriz[c][h - c];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    c = coluna;
+                }else if(valor == -1) c = coluna;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra++;
+        }
+
+        posicao_inicial_palavra = 0;
+    }
+
+    posicao_insercao_caracter = 0;
+    posicao_inicial_palavra = 0;
+
+    for(int h = coluna - 1; h >= 0; h--){
+
+        while(posicao_inicial_palavra < coluna){
+
+            for(int c = posicao_inicial_palavra; c <= h; c++){
+
+                palavra[posicao_insercao_caracter++] = matriz[h - c][c];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    c = coluna;
+                }else if(valor == -1) c = coluna;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra++;
+        }
+
+        posicao_inicial_palavra = 0;
+    }
+
+    posicao_insercao_caracter = 0;
+    posicao_inicial_palavra = coluna - 1;
+
+    for(int h = 1; h < coluna; h++){
+
+        while(posicao_inicial_palavra >= 0){
+
+            for(int c = posicao_inicial_palavra; c >= h; c--){
+
+                palavra[posicao_insercao_caracter++] = matriz[h + ((coluna - 1) - c)][c];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    c = -1;
+                }else if(valor == -1) c = -1;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra--;
+        }
+
+        posicao_inicial_palavra = coluna - 1;
+    }
+
+    posicao_insercao_caracter = 0;
+    posicao_inicial_palavra = coluna - 1;
+
+    for(int h = 1; h < coluna; h++){
+
+        while(posicao_inicial_palavra >= 0){
+
+            for(int c = posicao_inicial_palavra; c >= h; c--){
+
+                palavra[posicao_insercao_caracter++] = matriz[c][h + ((coluna - 1) - c)];
+                palavra[posicao_insercao_caracter] = '\0';
+                int valor = buscar_trie(arvore_trie, palavra);
+
+                if (valor == 1){
+                    *arvore_avl = inserir_no_avl(*arvore_avl, palavra);
+                    c = -1;
+                }else if(valor == -1) c = -1;
+            }
+
+            palavra[0] = '\0';
+            posicao_insercao_caracter = 0;
+            posicao_inicial_palavra--;
+        }
+
+        posicao_inicial_palavra = coluna - 1;
+    }
 }
--- src/main.c
+++ src/main.c
@@ -4,30 +4,33 @@
 int main(){
     char *nomeDoArquivoPalavras = "data/palavras.txt";
     char * nomeDoArquivoTabuleiro = "data/tabuleiro.txt";
-    No_trie *raiz = criar_no_trie();
-    No *raiz_avl = NULL;
-    ler_palavras(raiz, nomeDoArquivoPalavras);
-
-    /*const char *palavras[] = {"ganimedes", "calisto", "io", "metis", "adrasteia", "ananke", "carme", "pheme", "europa", "himalia", "elara", "pasifae", "sinope", "leda", "agape"}; 
-    for(int i = 0; i < 15; i++){
-        if(buscar_trie(raiz, palavras[i])) printf("Palavra %s encontrada!\n", palavras[i]);
-        else printf("Palavra %s não encontrada!\n", palavras[i]);
-    }*/
-
     int indices_matriz[2];
     char **matriz;
+    No_trie *raiz_trie = criar_no_trie();
+    No *raiz_avl = NULL;
+    
+    ler_palavras(raiz_trie, nomeDoArquivoPalavras);
 
     lerTamanhoDoTabuleiro(nomeDoArquivoTabuleiro, indices_matriz);
     alocarMatrizTabuleiro(&matriz, indices_matriz[0], indices_matriz[1]);
     ler_tabuleiro(nomeDoArquivoTabuleiro, indices_matriz[0], indices_matriz[1], matriz);
+    imprimirMatriz(matriz, indices_matriz[0], indices_matriz[1]);
 
-    busca_horizontal(matriz, indices_matriz[0], indices_matriz[1], raiz, &raiz_avl);
+    busca_horizontal(matriz, indices_matriz[0], indices_matriz[1], raiz_trie, &raiz_avl);
+    busca_vertical(matriz, indices_matriz[0], indices_matriz[1], raiz_trie, &raiz_avl);
+    busca_diagonal(matriz, indices_matriz[0], indices_matriz[1], raiz_trie, &raiz_avl);
     imprimir_em_ordem(raiz_avl);
 
+    busca_diagonal(matriz, indices_matriz[0], indices_matriz[1], raiz_trie, &raiz_avl);
+
     //Liberar memória
-    /*for (int i = 0; i < indices_matriz[0]; i++) {
+    for (int i = 0; i < indices_matriz[0]; i++) {
         free(matriz[i]);
-    }*/
+    }
+
+    free(matriz);
+    liberar_trie(raiz_trie);
+    liberar_avl(raiz_avl);
 
     return 0;
 }
